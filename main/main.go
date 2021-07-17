fd60b78e9cb9cd1edb8bdceee6066d57b49c54f0
Hongbin.Yuan <yuanhongbin9090@gmail.com>
2021-07-17 22:53:19 +0800

    update

--- main/main.go
+++ main/main.go
@@ -6,45 +6,116 @@ import (
 	"encoding/hex"
 	"fmt"
 	"strconv"
+	"strings"
 	"time"
 )
 
+// index for contacting two md5 string
+var contactIndex = []int{2, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 2, 2, 1, 1, 1, 1, 2, 2, 1, 1, 1, 2, 1, 1}
+
+// get password from this index
+var passwordLength = []int{5, 24, 19, 23, 2, 11, 20, 9, 3, 29, 17, 27, 6, 13, 31, 16}
+
 func main() {
-	stringInDb := "b-6S,c%Xxt!QmbZj56RjrM@3p"
-	indexArray := []uint8{1, 2, 4, 8, 16}
+	var seed string
+	for i := 0; i <= 10000; i++ {
+		fmt.Printf("The index: [%d]\n", i)
 
-	// calculate sha256
-	sha256Sum := sha256.Sum256([]byte(stringInDb))
-	hexOfSha256 := hex.EncodeToString(sha256Sum[:])
-	fmt.Printf("SHA256 of [%s] is %s\n", stringInDb, hexOfSha256)
+		// get random seed
+		seed = RandStringRunes(32)
 
-	// calculate md5
-	md5Sum := md5.Sum([]byte(hexOfSha256))
-	hexOfMd5 := hex.EncodeToString(md5Sum[:])
-	fmt.Printf("MD5 of [%s] is %s\n", hexOfSha256, hexOfMd5)
+		// get password and salt
+		originalPassword, salt := generateSalt(seed)
 
-	// get current nona second
-	secondOfNow := time.Now().UnixNano()
-	// calculate md5 of Nanosecond
-	md5SumOfSecond := md5.Sum([]byte(strconv.FormatInt(secondOfNow, 10)))
-	hexOfSecond := hex.EncodeToString(md5SumOfSecond[:])
-	fmt.Printf("MD5 of [%d] is %s\n", secondOfNow, hexOfSecond)
+		// get md5OfSeed and md5OfSecond from salt
+		passwordFromSalt := getPasswordFromSalt(salt)
 
-	// get substrate index
-	index, _ := strconv.ParseInt(string(hexOfSecond[0]), 16, 8)
-	subIndex := index + 1
-
-	//
-	newMd5HexOfSecond := hexOfSecond[subIndex:] + hexOfSecond[1:subIndex]
-	fmt.Printf("Substrate index: [%d] \n", index)
-	fmt.Printf("Firtst: [%s] \n", hexOfSecond[1:subIndex])
-	fmt.Printf("Second: [%s] \n", hexOfSecond[subIndex:])
-	fmt.Printf("New   : [%s] \n", newMd5HexOfSecond)
-	fmt.Printf("Len of new   : [%d] \n", len(newMd5HexOfSecond))
-
-
+		// log if password not equals
+		if strings.Compare(originalPassword, passwordFromSalt) != 0 {
+			fmt.Printf("Password not eqult for seed: [%s]\n", seed)
+			fmt.Printf("Salt: [%s]\n", salt)
+			fmt.Printf("OriginalPassword: [%s]\n", originalPassword)
+			fmt.Printf("PasswordFromSalt: [%s]\n", passwordFromSalt)
+			fmt.Println()
+		}
+	}
 }
 
-func getPasswordFromRandom(random string) {
+// generate salt
+func generateSalt(seed string) (string, string) {
+	// get md5 of seed
+	var sha256OfSeedResult [sha256.Size]byte
+	for i := 0; i <= 3; i++ {
+		sha256OfSeedResult = sha256.Sum256([]byte(seed))
+	}
+	md5SumOfSeed := md5.Sum([]byte(hex.EncodeToString(sha256OfSeedResult[:])))
+	md5HexOfSeed := hex.EncodeToString(md5SumOfSeed[:])
 
+	// get original originalPassword
+	originalPassword := getPassword(md5HexOfSeed)
+
+	// get md5 of current nanosecond
+	md5OfNanosecond := getMd5OfNanosecond()
+
+	// get split index for md5HexOfSeed
+	index := getSplitIndex(md5OfNanosecond)
+	// split md5HexOfSeed and recontact
+	splitMd5HexOfSeed := md5HexOfSeed[index:] + md5HexOfSeed[0:index]
+
+	var salt string
+	currentIndex := 0
+	for _, value := range contactIndex {
+		// contact salt
+		salt = salt +
+			splitMd5HexOfSeed[currentIndex : currentIndex+value] +
+			md5OfNanosecond[currentIndex : currentIndex+value]
+
+		currentIndex = currentIndex + value
+	}
+
+	return originalPassword, salt
+}
+
+func getPasswordFromSalt(salt string) string {
+	var md5HexOfSeed, md5HexOfNanosecond string
+
+	currentIndex := 0
+	for _, value := range contactIndex {
+		md5HexOfSeed = md5HexOfSeed + salt[currentIndex:currentIndex+value]
+		md5HexOfNanosecond = md5HexOfNanosecond + salt[currentIndex+value:currentIndex+value*2]
+
+		currentIndex = currentIndex + value*2
+	}
+
+	subIndex := getSplitIndex(md5HexOfNanosecond)
+	originalMd5OfSeed := md5HexOfSeed[int64(len(md5HexOfNanosecond))-subIndex:len(md5HexOfNanosecond)] +
+		md5HexOfSeed[0:(int64(len(md5HexOfNanosecond))-subIndex)]
+
+	return getPassword(originalMd5OfSeed)
+}
+
+//get password
+func getPassword(md5HexOfSeed string) string {
+	var password string
+	for _, index := range passwordLength {
+		password = password + md5HexOfSeed[index:index+1]
+	}
+	return password
+}
+
+// get split index
+func getSplitIndex(md5String string) int64 {
+	head, _ := strconv.ParseInt(string(md5String[0]), 16, 8)
+	tail, _ := strconv.ParseInt(string(md5String[len(md5String)-1]), 16, 8)
+	// index to split original md5OfSeed
+	return head + tail
+}
+
+func getMd5OfNanosecond() string {
+	// get current nona second
+	secondOfNow := time.Now().UnixNano()
+
+	// calculate md5 of Nanosecond
+	md5SumOfSecond := md5.Sum([]byte(strconv.FormatInt(secondOfNow, 10)))
+	return hex.EncodeToString(md5SumOfSecond[:])
 }
---
+++ main/random_util.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"math/rand"
+	"time"
+)
+
+const charset = "abcdefghijklmnopqrstuvwxyz" +
+	"ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789@#(#@IO!#!!)(!#!<><?#@"
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+var letterRunes = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
+func RandStringRunes(n int) string {
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = letterRunes[rand.Intn(len(letterRunes))]
+	}
+	return string(b)
+}
